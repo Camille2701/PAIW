69e68c7078cd54b1b80ba043663431dbe990b526
Clément KERVICHE <ckerviche@myges.fr>
2025-07-14 14:03:45 +0200

    fix: filament-conflicts

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,12 +24,6 @@ class Product extends Model
         'price' => 'decimal:2',
     ];
 
-    protected static function boot()
-    {
-        parent::boot();
-        Model::unguard();
-    }
-
     public function productType(): BelongsTo
     {
         return $this->belongsTo(ProductType::class);
@@ -56,6 +50,7 @@ class Product extends Model
                     $counter++;
                 }
             }
+            Model::unguard();
         });
     }
 
